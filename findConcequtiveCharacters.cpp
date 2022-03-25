af39e6e3b7bac63f23ac721843483055c2ab2247
Sirodo8plp <te01234@hotmail.com>
2022-03-25 17:19:07 +0200

    added comments.

--- findConcequtiveCharacters.cpp
+++ findConcequtiveCharacters.cpp
@@ -2,13 +2,54 @@
 #include <string>
 #include <cstring>
 #include <cstdlib>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
+
+/*
+We have used structs in order to shorten the lenth 
+of the arguments we pass in every function. In this way,
+we read our code faster and we make it easier to understand.
+
+sideValues has the index and count values of every substring we check.
+Ultimately, they give the final solution to the problem.
+*/
+
 struct sideValues
 {
     int index;
     int count;
 };
+
+/*
+The arguments struct is passed as an argument in every call of the recursive function.
+m is the number of concequtive characters we need to find.
+
+C is the character we have to check for.
+
+The originalString is used when the substring's length is less than m, and we have to 
+borrow m - substring's.length characters from the original string.
+
+subString is left or right substring we pass in the recursion every time.
+
+subStartIndex is the substring's index in the original String.
+
+Example: User gives the string "acdabcdacc", m = 3, C= c
+First call of FindConcequtiveCharacters:
+    m= 3, C=c, originalString="acdabcdacc",subString="acdabcdacc",subStartIndex= 0.
+Second call of FindConcequtiveCharacters:
+    m=3, C=c, originalString="acdabcdacc", subString="acdab",subStartIndex = 0.
+Third call of FindConcequtiveCharacters:
+    m=3, C=c, originalString="acdabcdacc", subString="acd",subStartIndex = 0.
+Fourth call of FindConcequtiveCharacters:
+    m=3,C=c,orinalString="acdabcdacc",subString="ab",subStartIndex = 3.
+Fifth call of FindConcequtiveCharacters:
+    m=3,C=c,originalString="acdabcdacc", subString="cdacc", subStartIndex = 5.
+.
+.
+.
+*/
 
 struct arguments {
     int m;
@@ -38,16 +79,26 @@ int main() {
 
     sideValues solutions;
     arguments args = {m,C,str,str,0};
+
+    auto start = high_resolution_clock::now();
     solutions = FindConcequtiveCharacters(args);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
     cout << "String given: " << str << endl
     << "Character to search: " << C << endl
     << "For " << m << " concequtive characters." << endl
     << "Solution." << endl << "Starting Index: " << solutions.index 
-    << endl << "Count: " << solutions.count << endl;
+    << endl << "Count: " << solutions.count << endl
+    << endl << "Duration: " << duration.count() << "microseconds" << endl;
     return 0;
 }
 
-// O(n)
+/*
+With this function we count the number of times character C appears
+in substring str.
+*/
+
 int CountCharactersInString(string str,char C) {
     int count = 0;
     for (int i=0; i < str.length(); i++) {
@@ -56,12 +107,35 @@ int CountCharactersInString(string str,char C) {
     return count;
 }
 
-//O(1)
+/*
+We use FindMiddle function to divide the string in two parts.
+If the string has an even length, substrings will have substring.length / 2 length.
+    Example: string is "abba". We get two substrings, "ab" and "ba".
+If the sring has an odd length, the left substring will have an odd length, while
+the right one will have an even.
+    Example: string is "abcba". The Left substring is "abc" and the right one is "ba".
+*/
+
 int FindMiddle(int length) {
     div_t result = div(length,2);
     if(result.rem == 1) return result.quot;
     return result.quot - 1;
 }
+
+/*
+We use the function FindCharactersFromCenter to check substrings that contain
+the middle character in a substring. We take two cases into consideration. The one 
+is when the substring's length and the m variable are even numbers. The other case is
+when both variables are odd numbers.
+    Example: Substring = "abba" and m=2. We check the substrings "ab" and "ba" with
+    the divide and conquer logic and we use FindCharactersFromCenter to also check the
+    substring "bb".
+
+    Another Example: If the substring is "abcba" and m equals 3, we will check the 
+    left substring "abc" and the right substring is "ba". As a result, for the right 
+    substring, we borrow one character from the left and we check "cba". Finally, with
+    FindCharactersFromCenter we check the substring "bcb".
+*/
 
 sideValues FindCharactersFromCenter(arguments args, int middle) {
     if(args.subString.length() % 2 == 0 && args.m % 2 == 0
@@ -83,29 +157,36 @@ sideValues FindCharactersFromCenter(arguments args, int middle) {
     return {0,0};
 }
 
+/*
+The Borrow function is used when we have split the string so much that eventually
+the substring's length that is checked is smaller that variable m. For example, if 
+the user's string is "abcba" and m equals 3, we split the string in substrings "abc" 
+and "ba". The substring "abc" has 3 characters and equals variable m. As a result, we
+search character C with the above-mentioned function and we return. However, the 
+substring "ba" has only two characters and needs to borrow one character from the left.
+Therefore, we check the substring "cba" and we return.
+
+Example: the string is "abcbafffac", m = 4, C=f.
+Substring "abc" must only borrow one character from the right.
+Substring "fac" must only borrow one character from the left.
+
+For substring "fff", we must check the substring "afff", the substring 
+"fffa", compare the two counts and return the correct index and count variables.
+leftBorrowCount is for the left borrow and rightBorrowCount is for the right one.
+*/
+
 sideValues Borrow(arguments args,int middle,int borrow) {
     if(args.subStartIndex == 0) 
-        //we need to borrow only from the right side;
         return {
             args.subStartIndex,
             CountCharactersInString(args.originalString.substr(0,args.m),args.C)
         };
     if(args.subStartIndex + args.m > args.originalString.length()) {
-        //we need to borrow only from the left side;
         return {
             args.subStartIndex - borrow,
             CountCharactersInString(args.originalString.substr(args.subStartIndex - borrow,args.m),args.C)
         };
     }
-    //we have to borrow both from left and right.
-    /*
-    Example case: The original string is cddabcdacc. m = 3
-    cddabcdacc -> a)cddab | b) cdacc
-    cddab -> a)cdd | b)ab
-    ab needs to borrow 1 character
-    We have to check the strings 'dab' and 'abc'
-    Then compare the counts and return appropriate values.
-    */
    int leftBorrowCount,rightBorrowCount;
    leftBorrowCount = CountCharactersInString(args.originalString.substr(args.subStartIndex-borrow,args.m),args.C);
    rightBorrowCount = CountCharactersInString(args.originalString.substr(args.subStartIndex,args.m),args.C);
@@ -119,6 +200,31 @@ sideValues Borrow(arguments args,int middle,int borrow) {
         rightBorrowCount
     };
 }
+
+/*
+FindConcequtiveCharacters is the divide and conquer function that we use.
+stringLength equals the substring's length.
+middle equals the middle index of the substring.
+
+substr method takes two arguments: The first argument is the starting index of the new 
+substring. The second arguments is for the length of the new substring.
+
+We want the left substring to have middle + 1 characters. 
+If the given string is "abba", middle will be 1 so the left substring starts 
+from index 0, and has middle + 1 = 1 + 1 = 2 characters.
+If the given string is "abcba", middle will equal 2. The left substring will then have
+middle + 1 = 2 + 1 = 3 characters.
+
+Right string starts from middle + 1 position and has stringLength - middle + 1 characters.
+As a result, if the string is "abba", the right substring will have subStartIndex=2 and
+stringLength - middle + 1 = 4 - 1 + 1 = 2 characters length.
+
+Finally, we call FindCharactersFromCenter to check the center substring.
+
+left,right and equal structs are for the left,right and center substring respectively.
+We finally compare the sides' count and return the right substring values.
+
+*/
 
 sideValues FindConcequtiveCharacters(arguments args) {
     int stringLength = args.subString.length();
